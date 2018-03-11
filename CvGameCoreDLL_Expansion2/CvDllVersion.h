e054901574dbff1335e3f197969b710a21560824
João Varandas <j.varandas7@gmail.com>
2018-03-11 14:46:33 +0000

    GUID and specific #defines
    Added new GUID, together with specific defines to keep track of modified
    functions. Code now compiles and can be exported as a playable mod

--- CvGameCoreDLL_Expansion2/CvDllVersion.h
+++ CvGameCoreDLL_Expansion2/CvDllVersion.h
@@ -7,6 +7,8 @@
 	------------------------------------------------------------------------------------------------------- */
 #pragma once
 
+#include "SI.h"
+
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 // WARNING! This must be unique for each DLL!
@@ -14,8 +16,14 @@
 // Use the Visual Studio Create GUID option in the Tools menu to create a new GUID.
 // {C3B2B6B3-439C-480b-8536-1CF39FCFC682}
 // {83F34FF0-3D60-43e7-AEDB-82C79867EE3F}
+
+#ifdef SI_GUID
+static const GUID CIV5_XP2_DLL_GUID =
+{ 0xfed09f46, 0x35fa, 0x447f, { 0xc6, 0x84, 0x69, 0xab, 0x04, 0x69, 0x87, 0x1d } };
+#else
 static const GUID CIV5_XP2_DLL_GUID = 
 { 0x83f34ff0, 0x3d60, 0x43e7, { 0xae, 0xdb, 0x82, 0xc7, 0x98, 0x67, 0xee, 0x3f } };
+#endif // SI_GUID
 
 static const char* CIV5_XP2_DLL_VERSION = "1.0.0";
 //------------------------------------------------------------------------------
---
+++ CvGameCoreDLL_Expansion2/SI.h
@@ -0,0 +1,14 @@
+// SI.h
+// Used to keep track of modified functions throughout the code
+
+#pragma once
+
+#ifndef SI_MODS_H
+#define SI_MODS_H
+
+// Random needed stuff
+
+// SI mod new GUID
+#define SI_GUID
+
+#endif
